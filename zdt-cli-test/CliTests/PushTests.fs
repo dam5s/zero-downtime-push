e8ea7cd35e959c7ee1609c9ebcd7dd4a07ae612a
Damien Le Berrigaud <damien@itsdamo.com>
2019-03-08 10:33:19 -0700

    Back-fill test for push rollback.

--- zdt-cli-test/CliTests/PushTests.fs
+++ zdt-cli-test/CliTests/PushTests.fs
@@ -19,10 +19,35 @@ let ``Running successful push`` () =
         out <- List.append out [ cmd args ]
         Ok ()
 
+
     let result = push cf "my-app" [ "-f" ; "my-manifest.yml" ]
+
 
     result |> shouldEqual (Ok ())
     out |> shouldEqual [ "cf rename my-app my-app-venerable"
                          "cf push my-app -f my-manifest.yml"
                          "cf delete my-app-venerable"
+                       ]
+
+
+[<Fact>]
+let ``Failing during push`` () =
+    let mutable out : string list = []
+
+    let cf (args : string list) : Result<unit, string> =
+        out <- List.append out [ cmd args ]
+
+        match List.head args with
+        | "push" -> Error "Push error"
+        | _ -> Ok ()
+
+
+    let result = push cf "my-app" [ "-f" ; "my-manifest.yml" ]
+
+
+    result |> shouldEqual (Error "Push error")
+    out |> shouldEqual [ "cf rename my-app my-app-venerable"
+                         "cf push my-app -f my-manifest.yml"
+                         "cf delete my-app"
+                         "cf rename my-app-venerable my-app"
                        ]
